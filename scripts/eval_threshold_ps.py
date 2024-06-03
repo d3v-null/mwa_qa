8a99003280013dd4eaba15b33df7ea5b291b8bf7
d3v-null <dev.null@curtin.edu.au>
2024-06-03 03:39:56 +0000

    update eval threshold display

--- scripts/eval_threshold_ps.py
+++ scripts/eval_threshold_ps.py
@@ -35,17 +35,21 @@ def read_wsstats(wsfile, obsids):
     df = pd.read_csv(wsfile, sep='\t')
     obs = np.unique(np.array(df['OBS']))
     inds = common_indices(obs, obsids)
-    return np.array(df['LST DEG'])[inds], np.array(df['CONFIG'])[inds]
+    return np.array(df['LST DEG'])[inds], np.array(df['CONF'])[inds]
 
+
+thresh_index=[
+    'P_WINDOW NOSUB',
+    "P_WIN_WEDGE RATIO NOSUB",
+    "P_WINDOW RATIO SUB",
+    "P_WEDGE RATIO SUB"
+]
 
 def evaluate_threshold(ewp, df):
     # calculating IQR (interquartile range)
 
     outdata = []
-    for i, fl in enumerate(['P_WINDOW NOSUB',
-                            "P_WIN_WEDGE RATIO NOSUB",
-                            "P_WINDOW RATIO SUB",
-                            "P_WEDGE RATIO SUB"]):
+    for i, fl in enumerate(thresh_index):
         if args.per_pointing:
             outdict = {}
             outdict['Metric'] = fl
@@ -74,23 +78,55 @@ def evaluate_threshold(ewp, df):
             outdata.append(outdict)
 
     if args.per_pointing:
-        df_out = pd.DataFrame(data=outdata)
+        df_out = pd.DataFrame(data=outdata, index=thresh_index)
 
     else:
-        df_out = pd.DataFrame(outdata, index=['P_WINDOW NOSUB',
-                                              "P_WIN_WEDGE RATIO NOSUB",
-                                              "P_WINDOW RATIO SUB",
-                                              "P_WEDGE RATIO SUB"],
+        df_out = pd.DataFrame(outdata, index=thresh_index,
                               columns=['Lthresh', 'Uthresh'])
 
     return df_out
 # plotting
 
+import matplotlib.pyplot as plt 
+from matplotlib import colormaps, colors
+import numpy as np
+import warnings
+
+def gen_color(cmap,n,reverse=False):
+    '''Generates n distinct color from a given colormap.
+
+    Args:
+        cmap(str): The name of the colormap you want to use.
+            Refer https://matplotlib.org/stable/tutorials/colors/colormaps.html to choose
+            Suggestions:
+            For Metallicity in Astrophysics: Use coolwarm, bwr, seismic in reverse
+            For distinct objects: Use gnuplot, brg, jet,turbo.
+
+        n(int): Number of colors you want from the cmap you entered.
+
+        reverse(bool): False by default. Set it to True if you want the cmap result to be reversed.
+
+    Returns: 
+        colorlist(list): A list with hex values of colors.
+    '''
+    c_map = colormaps.get_cmap(str(cmap)) # select the desired cmap
+    arr=np.linspace(0,1,n) #create a list with numbers from 0 to 1 with n items
+    colorlist=list()
+    for c in arr:
+        rgba=c_map(c) #select the rgba value of the cmap at point c which is a number between 0 to 1
+        clr=colors.to_hex(rgba) #convert to hex
+        colorlist.append(str(clr)) # create a list of these colors
+    
+    if reverse==True:
+        colorlist.reverse()
+    return colorlist
+
+
 
 def plot_pspecqa(dfm, df_out, ewp, figname=None):
-    colors = mcp.gen_color(cmap='rainbow', n=len(ewp))
+    colors = gen_color(cmap='rainbow', n=len(ewp))
     labels = [r'$P_{win} (unsub)$',
-              r'$\frac{P_{win}}{P_{wed}} (sub)$',
+              r'$\frac{P_{win}}{P_{wed}} (unsub)$',
               r'$P_{win} (\frac{sub}{unsub})$',
               r'$P_{wed}(\frac{sub}{unsub})$'
               ]
@@ -168,67 +204,50 @@ def plot_pspecqa(dfm, df_out, ewp, figname=None):
         pylab.show()
 
 
-df = pd.read_csv(args.tsvfile, sep='\t')
-df.dropna(axis=0, how='all', inplace=True)
-df.dropna(axis=1, how='all', inplace=True)
-obsids = np.unique(np.array(df['OBS']))
-lst, config = read_wsstats(args.wsfile, obsids)
+def get_sub_mask(df):
+    """
+    get indices for unsub metrics
+    """
+    names = np.unique(df['CAL NAME'])
+    sub_names = [name for name in names if 'sub' in name.lower()]
+    assert len(sub_names) == 1, 'Only one sub image is allowed'
+    sub_name = sub_names[0]
+    return np.array(df['CAL NAME'] == sub_name)
 
-ps_obs = df['OBS']
-ps_ewp = df['EWP']
-ps_pwedge = df['P_WEDGE']
-ps_pwindow = df['P_WINDOW']
-ps_all = df['P_ALL']
-ps_name = df['CAL NAME']
+def get_metrics_df(args):
+    df = pd.read_csv(args.tsvfile, sep='\t')
+    df.dropna(axis=0, how='all', inplace=True)
+    df.dropna(axis=1, how='all', inplace=True)
+    
+    sub_mask = get_sub_mask(df)
+    common_cols = [ 'P_WEDGE', 'P_WINDOW', 'P_ALL']
+    df_unsub = df.iloc[~sub_mask][['OBS', 'CONF', 'EWP'] + common_cols]
+    df_sub = df.iloc[sub_mask][['OBS'] + common_cols]
+    
+    dfm = pd.merge(df_unsub, df_sub, on='OBS', suffixes=(' NOSUB', ' SUB'))
+    
+    for sub in ['SUB']:
+        for pow in ['WEDGE', 'WINDOW']:
+            if {f"P_{pow} {sub}", f"P_{pow} NOSUB"}.issubset(set(dfm.columns)):
+                dfm[f'P_{pow} RATIO {sub}'] = dfm[f'P_{pow} {sub}'] / \
+                    dfm[f'P_{pow} NOSUB']
 
-# nosub
-ps_obs_nsub = np.array(ps_obs[1::2])
-ps_name_nsub = np.array(ps_name[1::2])
-ps_pwedge_nsub = np.array(ps_pwedge[1::2])
-ps_pwindow_nsub = np.array(ps_pwindow[1::2])
-ps_all_nsub = np.array(ps_all[1::2])
-ps_ewp_nsub = np.array(ps_ewp[1::2])
+    dfm['P_WIN_WEDGE RATIO NOSUB'] = dfm['P_WINDOW NOSUB'] / dfm['P_WEDGE NOSUB']
+    print(dfm)
+    return dfm
 
-# # sub =
-ps_obs_sub = np.array(ps_obs[0::2])
-ps_name_sub = np.array(ps_name[0::2])
-ps_pwedge_sub = np.array(ps_pwedge[0::2])
-ps_pwindow_sub = np.array(ps_pwindow[0::2])
-ps_all_sub = np.array(ps_all[0::2])
-ps_ewp_sub = np.array(ps_ewp[0::2])
-
-metrics_dict = {'OBSID': ps_obs_nsub,
-                'EWP': ps_ewp_nsub,
-                'LST': lst,
-                'CONFIG': config,
-                'P_WEDGE NOSUB': ps_pwedge_nsub,
-                'P_WINDOW NOSUB': ps_pwindow_nsub,
-                'P_ALL NOSUB': ps_all_nsub,
-                'P_WEDGE SUB': ps_pwedge_sub,
-                'P_WINDOW SUB': ps_pwindow_sub,
-                'P_ALL SUB': ps_all_sub
-                }
-
-dfm = pd.DataFrame(data=metrics_dict)
-
-for sub in ['SUB']:
-    for pow in ['WEDGE', 'WINDOW']:
-        if {f"P_{pow} {sub}", f"P_{pow} NOSUB"}.issubset(set(dfm.columns)):
-            dfm[f'P_{pow} RATIO {sub}'] = dfm[f'P_{pow} {sub}'] / \
-                dfm[f'P_{pow} NOSUB']
-
-dfm['P_WIN_WEDGE RATIO NOSUB'] = dfm['P_WINDOW NOSUB'] / dfm['P_WEDGE NOSUB']
-
-dfm['P_WIN_WEDGE RATIO NOSUB'] = dfm['P_WINDOW NOSUB'] / dfm['P_WEDGE NOSUB']
+dfm = get_metrics_df(args)
 
 if args.outfile is None:
     outfile = args.tsvfile.replace('.tsv', '_thresholds.tsv')
 else:
     outfile = args.outfile
+    
+dfm.to_csv(outfile, sep='\t', index=False)
 
 # splitting into configuration
 # phase I
-df_ph1 = dfm.iloc[np.where(dfm['CONFIG'] == 'Phase I')]
+df_ph1 = dfm.iloc[np.where(dfm['CONF'] == 'Phase I')]
 ewp_ph1 = np.unique(df_ph1['EWP'])
 df_out_ph1 = evaluate_threshold(ewp_ph1, df_ph1)
 
@@ -243,7 +262,7 @@ else:
     df_out_ph1.to_csv(outfile.replace('.tsv', '_PH1.tsv'),
                       index=False, sep='\t')
 # phase II
-df_ph2 = dfm.iloc[np.where(dfm['CONFIG'] == 'Phase II Compact')]
+df_ph2 = dfm.iloc[np.where(dfm['CONF'] == 'Phase II Compact')]
 ewp_ph2 = np.unique(df_ph2['EWP'])
 df_out_ph2 = evaluate_threshold(ewp_ph2, df_ph2)
 
@@ -257,3 +276,23 @@ if args.per_pointing:
 else:
     df_out_ph2.to_csv(outfile.replace('.tsv', '_PH2.tsv'),
                       index=False, sep='\t')
+
+for phase, df_out, ewps in [
+        ("ph1", df_out_ph1, ewp_ph1), 
+        ("ph2", df_out_ph2, ewp_ph2),
+]:
+    for ewp in ewps:
+        print(f"Thresholds for {phase} {ewp=}")
+        thresh = df_out.loc['P_WINDOW NOSUB', ewp]
+        print(f"    filter_max_ps_window_unsub={thresh[1]:6.4f}")
+        print(f"    filter_min_ps_window_unsub={thresh[0]:6.4f}")
+        thresh = df_out.loc["P_WIN_WEDGE RATIO NOSUB", ewp]
+        print(f"    filter_max_ps_ratio_unsub={thresh[1]:6.4f}")
+        print(f"    filter_min_ps_ratio_unsub={thresh[0]:6.4f}")
+        thresh = df_out.loc["P_WINDOW RATIO SUB", ewp]
+        print(f"    filter_max_ps_win_ratio_sub={thresh[1]:6.4f}")
+        print(f"    filter_min_ps_win_ratio_sub={thresh[0]:6.4f}")
+        thresh = df_out.loc["P_WEDGE RATIO SUB", ewp]
+        print(f"    filter_max_ps_wedge_ratio_sub={thresh[1]:6.4f}")
+        print(f"    filter_min_ps_wedge_ratio_sub={thresh[0]:6.4f}")
+        print("")
